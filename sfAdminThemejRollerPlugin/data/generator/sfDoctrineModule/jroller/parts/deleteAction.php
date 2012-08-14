bfff69ecdfa31bf325489a0d76436849b8f2dc39
fi76 <fi76account@gmail.com>
2012-08-14 13:14:41 +0200

    nestedset layout delete

--- sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/parts/deleteAction.php
+++ sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/parts/deleteAction.php
@@ -4,9 +4,6 @@
 
     $this->dispatcher->notify(new sfEvent($this, 'admin.delete_object', array('object' => $this->getRoute()->getObject())));
 
-<?php if ($this->configuration->getValue('list.layout') == 'nestedset'): ?>    
-    $this->getRoute()->getObject()->getNode()->delete();
-<?php endif; ?>
     $this->getRoute()->getObject()->delete();
 
     $this->getUser()->setFlash('notice', 'The item was deleted successfully.');
