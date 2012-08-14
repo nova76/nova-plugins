c991bce1afcd0a7203de399adf6bf3543983974d
fi76 <fi76account@gmail.com>
2012-08-14 14:06:02 +0200

    flash delete after echo

--- sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/template/templates/_flashes.php
+++ sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/template/templates/_flashes.php
@@ -2,7 +2,8 @@
 [?php if ($sf_user->hasFlash('notice')): ?]
   <div class="notice ui-state-highlight ui-corner-all">
     <span class="ui-icon ui-icon-info floatleft"></span>&nbsp;
-    [?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]
+    [?php echo __($sf_user->getFlash('notice', ESC_RAW), array(), 'sf_admin') ?]
+    [?php $sf_user->setFlash('notice', null) ?]
   </div>
 [?php endif; ?]
 
@@ -10,6 +11,7 @@
   <div class="error ui-state-error ui-corner-all">
     <span class="ui-icon ui-icon-alert floatleft"></span>&nbsp;
     [?php echo __($sf_user->getFlash('error', ESC_RAW), array(), 'sf_admin') ?]
+    [?php $sf_user->setFlash('error', null) ?]
   </div>
 [?php endif; ?]
 </div>
