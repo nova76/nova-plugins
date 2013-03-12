4ee49e522f7be2526a34daba83f91626a315977a
fi76 <fi76account@gmail.com>
2013-03-12 23:31:52 +0100

    uj elem letrehozasakor, ha nincs root akkor letrehozzuk

--- sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/parts/newAction.php
+++ sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/parts/newAction.php
@@ -4,8 +4,8 @@
     $this-><?php echo $this->getSingularName() ?> = $this->form->getObject();
     
     <?php if ($this->configuration->getValue('list.layout') == 'nestedset'): ?>
-    $parent = $this->getRoute()->getObject();
-    $this->form->setDefault('root_id', $parent->getRootId()); 
+    $parent = $request->hasParameter('id') ? $this->getRoute()->getObject() :$this->makeRoot();
+    $this->form->setDefault('root_id', $parent->getId()); 
     <?php endif ?>    
 
     $this->dispatcher->notify(new sfEvent($this, 'admin.new_object', array('object' => $this-><?php echo $this->getSingularName() ?>)));
